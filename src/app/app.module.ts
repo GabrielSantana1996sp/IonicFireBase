174f2c88517b9c0e21a1de9b394cf8813e7ed9d6
GabrielSantana1996sp <gabrielsantanati@outlook.com>
2026-03-30 21:58:03 -0300

    Integrate Firebase and Firestore in AppModule
    Added Firebase initialization and Firestore provider to AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { environment } from './../environments/environment.prod';
+import { initializeApp } from 'firebase/app';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -7,10 +9,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//
+import {initializeApp,provideFirebaseApp} from '@angular/fire/app';
+import {getFirestone,provideFirestone} from '@angular/fire/firestone';
+
+import {environment} from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    provideFirebaseApp(()=> initializeApp(environment,firebaseConfig)),
+    provideFirestone(()=>getFirestone())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
